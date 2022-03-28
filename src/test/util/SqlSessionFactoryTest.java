b862d70530b3f69227b64bb58708244d1ebbaa09
eomingyu <djalsrb123@naver.com>
2022-03-28 16:53:52 +0900

    jsp2 수업 1일차

---
+++ src/test/util/SqlSessionFactoryTest.java
@@ -0,0 +1,15 @@
+package test.util;
+
+import org.apache.ibatis.session.SqlSessionFactory;
+
+import mybatis.SqlSessionBean;
+
+public class SqlSessionFactoryTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SqlSessionFactory factory = SqlSessionBean.getSessionFactory();
+		System.out.println(factory);
+	}
+
+}
